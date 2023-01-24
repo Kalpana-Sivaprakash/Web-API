9b2bc15542674782fe0b54d0904126696a6e4ab5
kalpana <kalpanarajasekar27@gmail.com>
2023-01-24 23:37:16 +0000

    updated folders

---
+++ questions.js
@@ -0,0 +1,25 @@
+let questions = [
+{
+    "title": "Which of the following methods is used to access HTML elements using Javascript?", 
+    "choices": ["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the above"],
+    "answer": "Both A and B"
+},
+
+{
+"title": "Upon encountering empty statements, what does the Javascript Interpreter do?",
+"choices": ["Throws an error", "Ignores the statements", "Gives a warning", "None of the above"],
+"answer": "Ignores the statements"
+},
+
+{
+    "title": "What keyword is used to check whether a given property is valid or not?",
+    "choices": ["in", "is in", "exists", "lies"],
+    "answer": "in"
+},
+
+{
+    "title": "Which function is used to serialize an object into a JSON string in Javascript?",
+    "choices": ["stringify()", "parse()", "convert()", "None of the above"],
+    "answer": "stringify()"
+}
+]
